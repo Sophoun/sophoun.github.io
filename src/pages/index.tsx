068cc6cae6a944cd62b480f5c6b7373a86393c27
Sophoun <sophoun@IT-NSOPHOUN.local>
2022-07-07 23:58:14 +0700

    add firebase anatics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,16 @@
-import React from "react";
+import { getAnalytics, logEvent } from "firebase/analytics";
+import { getApp } from "firebase/app";
+import firebase from "gatsby-plugin-firebase";
+import React, { useEffect } from "react";
 import Welcome from "../component/welcome";
 import Layout from "../layout/layout";
+import { analytics } from "../utils/firebase";
 
 const IndexPage = () => {
+
+	useEffect(() => {
+		logEvent(analytics, "Success calling from development.")
+	}, []);
 
 	return (
 		<div className="home-background">
---
+++ src/utils/firebase.ts
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDzveiaBNmYa8n1pGYZCFirWip1gdpKwok",
+    authDomain: "sophoun-project.firebaseapp.com",
+    projectId: "sophoun-project",
+    storageBucket: "sophoun-project.appspot.com",
+    messagingSenderId: "457109583638",
+    appId: "1:457109583638:web:0fd680d5a31a6c728c0237",
+    measurementId: "G-9LWDE07TL5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const analytics = getAnalytics(app);
